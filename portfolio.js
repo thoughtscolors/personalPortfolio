cadd364da0607374f9177df86f5478ae52077d27
Brady <vanderbannond@gmail.com>
2018-02-07 19:56:46 -0800

    testing js mouseover functionality working

--- portfolio.js
+++ portfolio.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (event) {
   started()
 })
 
-var started = function () {
+var started = function (event) {
   let photos = document.getElementsByClassName("photos")
   console.log(photos);
   console.log("started OK");
   let left = photos[0];
   let right = photos[1];
-  left.addEventListener("onmouseover", textOverlay);
-  right.addEventListener("onmouseover", textOverlay);
+  left.addEventListener("mouseover", textOverlay);
+  right.addEventListener("mouseover", textOverlay);
 }
 
 var textOverlay = function (event) {
-  let insert = document.getElementsByClassName("main-container")[0]
+  let insert = document.getElementsByClassName("photos")[0]
   console.log(insert);
   let div = document.createElement("div")
   div.classList.add("test")
   console.log(div);
-  insert.appendChild(div);
+  insert.append(div);
 }
