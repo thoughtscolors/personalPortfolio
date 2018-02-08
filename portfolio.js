c39a96b1253c6be080addb6a748b726c05d5c99c
Brady <vanderbannond@gmail.com>
2018-02-07 19:50:07 -0800

    basic layout done, working on testing extra js functionality

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,22 @@
-//javascript!
+document.addEventListener("DOMContentLoaded", function () {
+  started()
+})
+
+var started = function () {
+  let photos = document.getElementsByClassName("photos")
+  console.log(photos);
+  console.log("started OK");
+  let left = photos[0];
+  let right = photos[1];
+  left.addEventListener("onmouseover", textOverlay);
+  right.addEventListener("onmouseover", textOverlay);
+}
+
+var textOverlay = function (event) {
+  let insert = document.getElementsByClassName("main-container")[0]
+  console.log(insert);
+  let div = document.createElement("div")
+  div.classList.add("test")
+  console.log(div);
+  insert.appendChild(div);
+}
